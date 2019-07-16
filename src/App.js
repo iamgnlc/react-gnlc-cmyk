ea0f2571f44eb381dc538e899389add9db475b72
GNLC <gianluca.agnocchetti@gmail.com>
2019-07-16 17:39:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import g from "./assets/g.svg";
+import n from "./assets/n.svg";
+import l from "./assets/l.svg";
+import c from "./assets/c.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cmyk">
+      <div className="portion cyan">
+        <img className="letter g" src={g} alt="g" />
+      </div>
+      <div className="portion magenta">
+        <img className="letter n" src={n} alt="n" />
+      </div>
+      <div className="portion yellow">
+        <img className="letter l" src={l} alt="l" />
+      </div>
+      <div className="portion black">
+        <img className="letter c" src={c} alt="c" />
+      </div>
     </div>
   );
 }
