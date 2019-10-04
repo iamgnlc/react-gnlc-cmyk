ab1b9cecc05cb74e05ba1c44a92510399313845c
GNLC <gianluca.agnocchetti@gmail.com>
2019-10-04 12:07:22 +0100

    animations

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,16 @@
 import React from "react"
 
 import "../assets/styles/App.scss"
-import g from "../assets/images/g.svg"
-import n from "../assets/images/n.svg"
-import l from "../assets/images/l.svg"
-import c from "../assets/images/c.svg"
 
 import Head from "./Head"
+import GNLC from "./GNLC"
 import Repo from "./Repo"
-
-const letters = [
-  { key: "g", img: g },
-  { key: "n", img: n },
-  { key: "l", img: l },
-  { key: "c", img: c },
-]
 
 const App = React.memo(() => {
   return (
     <div className="cmyk">
       <Head />
-      {letters.map((letter, i) => {
-        return (
-          <div key={i} className={`letter ${letter.key}`}>
-            <img src={letter.img} alt={letter.key} />
-          </div>
-        )
-      })}
+      <GNLC />
       <Repo />
     </div>
   )
---
+++ src/components/GNLC.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+import "animate.css"
+
+import g from "../assets/images/g.svg"
+import n from "../assets/images/n.svg"
+import l from "../assets/images/l.svg"
+import c from "../assets/images/c.svg"
+
+const letters = [
+  { key: "g", img: g },
+  { key: "n", img: n },
+  { key: "l", img: l },
+  { key: "c", img: c },
+]
+
+const GNLC = React.memo(() => {
+  return letters.map((letter, i) => {
+    return (
+      <div key={i} className={`animated fadeInRight letter ${letter.key}`}>
+        <img
+          src={letter.img}
+          alt={letter.key}
+          className="animated fadeInLeft"
+        />
+      </div>
+    )
+  })
+})
+
+export default GNLC
