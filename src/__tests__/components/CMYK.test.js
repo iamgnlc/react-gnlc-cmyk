c5f4bbba9946fbafe294a9538a868e412bd9d34a
iamgnlc <gianluca.agnocchetti@gmail.com>
2022-01-03 18:14:23 +0000

    test

--- src/__tests__/components/CMYK.test.js
+++ src/__tests__/components/CMYK.test.js
@@ -1,12 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import App from "../../components/App"
+import CMYK from "../../components/CMYK"
 
-describe("App component", () => {
+describe("CMYK component", () => {
   it("should render without crashing", () => {
     const div = document.createElement("div")
-    ReactDOM.render(<App />, div)
+    ReactDOM.render(<CMYK />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
