4909b9b83baff3aaa55a954c892ba0bf9b17b97f
iamgnlc <gianluca.agnocchetti@gmail.com>
2023-11-07 07:48:38 +0000

    update test

--- src/__tests__/components/Repo.test.jsx
+++ src/__tests__/components/Repo.test.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 import Repo from '../../components/Repo';
 
 describe('Repo', () => {
-  it('should render correctly', () => {
+  it('should render correctly without showing', () => {
     const view = render(<Repo />);
+
+    expect(view.container).toMatchSnapshot();
+  });
+  it('should render correctly showing', () => {
+    const view = render(<Repo />);
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { keyCode: 93 });
+      document.dispatchEvent(event);
+    });
 
     expect(view.container).toMatchSnapshot();
   });
