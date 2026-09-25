bc798ff5d60088c03a2be3ea05728fb47f37256a
iamgnlc <gianluca.agnocchetti@gmail.com>
2026-09-25 22:11:47 +0100

    remove loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import packageInfo from '../package.json';
 import CacheBuster from './components/CacheBuster';
 import CMYK from './components/CMYK';
-import Loading from './Loading';
 import { isEnv } from './utils';
 
 const App = () => (
   <CacheBuster
     currentVersion={packageInfo.version}
     isEnabled={isEnv('production')}
-    loadingComponent={<Loading />}
   >
     <CMYK />
   </CacheBuster>
